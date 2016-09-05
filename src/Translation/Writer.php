00f8247f8529d3603187eb2b1bb48ded1f03705d
Nassif Bourguig <nbourguig@gmail.com>
2016-09-05 22:02:19 +0200

    Fix Laravel 5.1 support (dirname on Filesystem)

--- src/Translation/Writer.php
+++ src/Translation/Writer.php
@@ -60,7 +60,7 @@ class Writer
 
         $content = "<?php\n\nreturn ".Util::varExport($items).";\n";
 
-        if (! $this->files->isDirectory($dir = $this->files->dirname($file))) {
+        if (! $this->files->isDirectory($dir = dirname($file))) {
             $this->files->makeDirectory($dir, 0755, true);
         }
 
