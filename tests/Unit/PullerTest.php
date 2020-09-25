072ded3aeae20d9b95f19bbc58fee851ea0e95f8
Nassif Bourguig <nbourguig@gmail.com>
2020-09-25 16:38:01 +0200

    fix test

--- tests/Unit/PullerTest.php
+++ tests/Unit/PullerTest.php
@@ -16,6 +16,7 @@ class PullerTest extends TestCase
     {
         $translationSheet = Mockery::mock(TranslationsSheet::class);
         $translationSheet->shouldReceive('getSpreadsheet')->once()->andReturn($this->helper->spreadsheet());
+        $translationSheet->shouldReceive('getTitle')->once();
         $translationSheet->shouldReceive('readTranslations')->once();
 
         $writer = Mockery::mock(Writer::class);
